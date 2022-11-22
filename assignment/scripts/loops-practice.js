0ed87ac70a0abf4e64fd81d9b838b8c71e22cf1d
sezasada <111367099+sezasada@users.noreply.github.com>
2022-11-22 10:51:34 -0800

    Update loops-practice.js
    1.a 1.b done.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,10 +13,17 @@ for (let i = 0; i < 4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-
+// You need to change i < 4; - should be i < 6; 
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+}
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+// you need to change the value of i to start at 3 instead of 0.
+for (let i = 3; i < 6; i++) {
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
